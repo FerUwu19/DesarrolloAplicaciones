71eda9cfd432d14abe54b113754cf16b46a151b4
FerUwu19 <114791752+FerUwu19@users.noreply.github.com>
2023-06-18 16:36:04 -0600

    Semana 4 arreglos y casi toda semana 5

---
+++ TiendaV/src/main/java/com/tienda/tienda/Service/CategoriaService.java
@@ -0,0 +1,11 @@
+package com.tienda.tienda.service;
+
+import com.tienda.tienda.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
