c1db80a6d9bc549bdd1c304dcee801761f96c0f2
FerUwu19 <114791752+FerUwu19@users.noreply.github.com>
2023-07-16 22:07:48 -0600

    correciones y semana 9 x2

--- TiendaV/src/main/java/com/tienda/tienda/controller/ProductoController.java
+++ TiendaV/src/main/java/com/tienda/tienda/controller/ProductoController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -67,4 +68,5 @@ public class ProductoController {
         model.addAttribute("producto", producto);
         return "redirect:/producto/listado";
     }
+   
 }
